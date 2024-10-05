07580e53b5c0656e2eda6f496ec08f5f3f071e7c
LaxmanChoudhary <laxmanchoudhary6811@gmail.com>
2024-10-05 19:07:01 +0530

    db url fix

--- src/config/development.py
+++ src/config/development.py
@@ -3,6 +3,5 @@ import os
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'dev.sqlite')
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres.dlkmllgjnugadfulpsbk:cbA7lWddCxl51cjH@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'dev.sqlite')
